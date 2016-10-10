cde0a0adca5a01d693189bb2395293dae80fd470
Knut Kirkhorn <knutakir@student.hist.no>
2016-10-10 21:22:13 +0200

    Litt mer fiks
    fiks

--- Øving 8/src/Heap.java
+++ Øving 8/src/Heap.java
@@ -2,6 +2,46 @@
  * Created by Knut on 10.10.2016.
  */
 public class Heap {
+    int len;
+    int[] node;
 
+    int over(int i) {
+        return (i - 1) >> 1;
+    }
 
+    int venstre(int i) {
+        return (i << 1) + 1;
+    }
+
+    int hoyre(int i) {
+        return (i+1) << 1;
+    }
+
+    public void bytt(int[] tabell, int i, int j) {
+        int backup = tabell[j];
+        tabell[j] = tabell[i];
+        tabell[i] = backup;
+    }
+
+    public void fiksHeap(int i) {
+        int m = venstre(i);
+        if (m < len) {
+            int h = m + 1;
+            if (h < len && node[h] > node[m]) {
+                m = h;
+            }
+
+            if (node[m] > node[i]) {
+                bytt(node, i, m);
+                fiksHeap(m);
+            }
+        }
+    }
+
+    public void lagHeap() {
+        int i = len / 2;
+        while (i-- > 0) {
+            fiksHeap(i);
+        }
+    }
 }
--- Øving 8/src/Main.java
+++ Øving 8/src/Main.java
@@ -1,10 +1,13 @@
 /**
  * Created by Knut on 10.10.2016.
+ * Oppgave: http://www.aitel.hist.no/fag/_alg/v-graf/opg-v-graf.pdf
  */
 
-//Oppgave: http://www.aitel.hist.no/fag/_alg/v-graf/opg-v-graf.pdf
 public class Main {
     public static void main(String[] args) {
+        String filPlassering = "C:\\Users\\Knut\\Documents\\GitHub\\Algoritmer_og_datastrukturer\\Øving 8\\resources\\graf.txt";
+        Dijkstragraf dijkstragraf = new Dijkstragraf(filPlassering);
+        dijkstragraf.skrivut();
 
     }
 }
